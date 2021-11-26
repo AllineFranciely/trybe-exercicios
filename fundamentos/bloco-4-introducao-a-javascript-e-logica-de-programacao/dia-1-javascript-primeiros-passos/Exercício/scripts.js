38e085c0e96b0088c7ac3bd0e7366f89c6f0f73c
Alline Franciely <alline_franciely@hotmail.com>
2021-11-26 17:30:25 -0300

    exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício/scripts.js
@@ -44,13 +44,28 @@ if (recebeNumero > 0) {
 const anguloUm = -45;
 const anguloDois = 90;
 const anguloTres = 45;
-const invalido;
 const triangulo = (anguloUm + anguloDois + anguloTres);
 if (triangulo === 180) {
     console.log("true");
 }else {
     console.log("false");
 }
-if (anguloUm < 0) {
-    anguloUm = invalido
+
+//Exercíci0 6 rei, rainha, bispo, cavalo, torre, peão 
+const pecaXadres = "Cavalo";
+let result = pecaXadres.toLowerCase();
+if (result === "torre") {
+    console.log("Vertical e horizontal");
+} else if (result === "cavalo") {
+    console.log("Movimento em L");
+} else if (result === "bispo") {
+    console.log("Diagonal")
+} else if (result === "rainha") {
+    console.log("Vertical, horizontal e diagonal");
+} else if (result === "rei") {
+    console.log("1 casa vertical e horizontal")
+} else if (result === "peão") {
+    console.log("1 casa vertical")
+} else {
+    console.log("Peça inválida")
 }
