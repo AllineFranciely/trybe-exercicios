9319b1689b5b0ac2f10644ab8fea877b5909b4fd
Alline Franciely <alline_franciely@hotmail.com>
2021-11-28 16:41:24 -0300

    correção exercício 11

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício/Exercício 11/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Exercício/Exercício 11/scripts.js
@@ -16,13 +16,13 @@ salarioBase = (salarioBruto-inss);
 if (salarioBase < 1903.98) {
     IR = 0;
 } else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
-    IR = ((0,075 * salarioBase) - 142.80);
+    IR = ((0.075 * salarioBase) - 142.80);
 }else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
-    IR = ((0,15 * salarioBase) - 354.80);
+    IR = ((0.15 * salarioBase) - 354.80);
 } else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
-    IR = ((0,225 * salarioBase) - 636.13);
+    IR = ((0.225 * salarioBase) - 636.13);
 } else if (salarioBase > 4664.68) {
-    IR = ((0,275 * salarioBase) - 869.36);
+    IR = ((0.275 * salarioBase) - 869.36);
 }
 salarioLiquido = (salarioBase - IR);
 console.log(inss);
