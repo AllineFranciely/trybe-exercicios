173aeeb2600ff8624b1f01e40e1abd57616929fb
Alline Franciely <alline_franciely@hotmail.com>
2021-11-29 18:39:25 -0300

    exercícios 8 e 9 e bonus

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercícios bônus/scripts.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+numbers.sort(function(a,b){
+    return a - b;
+});
+console.log(numbers);
+
+let numeros = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+numeros.sort(function(a,b){
+    return b - a ;
+});
+console.log(numeros);
