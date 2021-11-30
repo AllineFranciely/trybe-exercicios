82cbbf22a262329e1e434c6edc165d3c6a08cc61
Alline Franciely <alline_franciely@hotmail.com>
2021-11-30 15:32:00 -0300

    exercícios 1 e 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,9 @@
+let n = 5;
+simbolo = "*";
+let inputLine = "";
+for (let index = 0; index < n; index +=1) {
+    inputLine = inputLine + simbolo;
+}
+for (let index = 0; index < n; index +=1) {
+        console.log(inputLine);
+}
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,6 @@
+const n = 9;
+let resultado = 100;
+for (let i = 0; i<=n; i+=1) {
+    resultado -=i;
+}
+console.log(resultado);
