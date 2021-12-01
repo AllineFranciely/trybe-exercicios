6308bb416285912d6bb8717ac0febb6cd1a9c402
Alline Franciely <alline_franciely@hotmail.com>
2021-12-01 15:37:15 -0300

    exercicios de fixaçao sobre for in

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objetos.js
@@ -3,7 +3,7 @@ let player = {
     lastName: "Silva",
     age: 34,
     medals: {golden: 2, silver: 3},
-    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018],
+    bestInTheWorld: [2006, 2007, 2008, 2009, 2010, 2018]
 };
 console.log("A jogadora " + player.name + " " + player.lastName + " tem " + player.age + " anos de idade.");
 console.log("A jogadora " + player.name + " " + player.lastName + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length + " vezes.");
