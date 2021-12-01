751702b3438a2c9c67571c0442cf90ca0ce31a40
Alline Franciely <alline_franciely@hotmail.com>
2021-12-01 17:11:10 -0300

    exercicio de fixaçao 1 sobre funçoes

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -0,0 +1,18 @@
+function operacoesComNumeros(a, b) {
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a * b);
+    console.log(a / b);
+    console.log(a % b);
+}
+operacoesComNumeros(30, 5);
+
+
+function operacoesNumericas(a, b) {
+    return (a + b);
+    return (a - b);
+    return (a * b);
+    return (a / b);
+    return (a % b);
+}
+console.log(operacoesNumericas(30,5));
