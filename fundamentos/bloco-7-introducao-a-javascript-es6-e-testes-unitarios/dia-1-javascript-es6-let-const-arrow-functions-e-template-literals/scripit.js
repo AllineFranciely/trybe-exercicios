dea80afe860a40832105f0784fd191c89a6e2247
Alline <alline_franciely@hotmail.com>
2022-01-12 18:35:29 -0300

    exercicio 2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scripit.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scripit.js
@@ -12,3 +12,44 @@
 
 testingScope(true);
 
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
+
+  return oddsAndEvens
+}
+
+const sortedArray = sortOddsAndEvens();
+console.log(` Os números ${sortedArray} estão ordenados de forma crescente`);
+
+//Parte 2
+//Exercício 1:
+
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
+console.log(factorial(4));
+
+//Exercício 2:
+
+const longestWord = text => {
+  let wordArray = text.split(' ');
+  let maxLength = 0;
+  let result = '';
+
+  for (const word of wordArray) {
+      if (word.length > maxLength) {
+          maxLength = word.length;
+          result = word;
+      }
+  }
+
+  return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
