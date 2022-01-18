37fee20f92d7fd0dc9630d5e2d311a1741ee77dc
Alline <alline_franciely@hotmail.com>
2022-01-18 16:20:44 -0300

    exercicio do dia - 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/Exercícios/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/Exercícios/script.js
@@ -13,3 +13,14 @@ const newEmployees = (callback) => {
   return employees;
 };
 console.log(newEmployees(pessoaContratada));
+
+//Exercício 2: 
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, callback) => {
+  const number = Math.floor((Math.random()*5) +1);
+
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try again!';
+};
+
+console.log(lotteryResult(2, numberChecker));
