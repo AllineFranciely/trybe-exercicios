d1ef02b1eb232fd925976bcef5ddbd65fae6e197
Alline <alline_franciely@hotmail.com>
2022-01-19 15:56:28 -0300

    exercicios para fixar forEach e find

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/Para fixar/script.js
@@ -0,0 +1,52 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
+
+//Exercício array.forEach: 
+const emailListInData = [
+  'roberta@email.com',
+  'paulo@email.com',
+  'anaroberta@email.com',
+  'fabiano@email.com',
+];
+
+const showEmailList = (email) => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
+};
+
+emailListInData.forEach(showEmailList);
+
+// Exercícios array.find : 
+// 1: 
+const numbersFind = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  return numbersFind.find((number) => number % 5 === 0 && number % 3 === 0);
+}
+console.log(findDivisibleBy3And5())
+
+// 2: 
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const findNameWithFiveLetters = () => {
+  return names.find((name) => name.length === 5);
+}
+
+console.log(findNameWithFiveLetters());
+
+// 3: 
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+function findMusic(id) {
+  return musicas.find((musica) => musica.id === id);
+}
+
+console.log(findMusic('31031685'))
