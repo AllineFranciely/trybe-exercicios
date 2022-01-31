946bfd21a7fee7ab0079d20cfd00e3404925c3ad
Alline <alline_franciely@hotmail.com>
2022-01-31 19:46:26 -0300

    exercicios do dia

---
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/coinsScript.js
@@ -0,0 +1,29 @@
+// api.js
+const fetchCoins = async () => {
+  const url = 'https://api.coincap.io/v2/assets';
+
+  const coins = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => error.toString());
+
+  return coins;
+}
+
+const setCoins = async () => {
+  const coins = await fetchCoins();
+
+  const coinsList = document.getElementById('coins-list');
+
+  coins.filter((coin) => Number(coin.rank( <= 10)
+  .forEach((coin) => {
+    const newLi = document.createElement('li');
+    
+
+    newLi.innerText = `${coin.name} (${coin.symbol}): ${usdPrice.toFixed(2)}`;
+
+    coinsList.appendChild(newLi);
+  });
+}
+
+window.onload = () => setCoins();
