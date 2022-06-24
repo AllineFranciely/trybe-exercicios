bd26af3a78faab2ff822fe4788b3fc8e296b428d
Alline <alline_franciely@hotmail.com>
2022-06-24 16:48:15 -0300

    exercicio 2

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/user', middlewares.createUser);
+app.get('/user', middlewares.getAllUsers);
+app.get('/user/:id', middlewares.findUserById);
 
 app.use(middlewares.error);
 
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/models/User.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/users-manager/models/User.js
@@ -17,4 +17,24 @@ function create({ firstName, lastName, email, password }) {
   .then(([result]) => ({ id: result.insertId, firstName, lastName, email }));
 }
 
-module.exports = { formatUser, create };
+function findAll() {
+  return connection.execute('SELECT * from users')
+  .then(([results]) => results.map(formatUser));
+}
+
+function findById(id) {
+	return connection.execute('SELECT * FROM users WHERE id = ?', [id])
+		.then(([results]) => {
+		if (results[0]) return formatUser(results[0]);
+		return null;
+		});
+}
+
+async function updateUser(id, { firstName, lastName, email, password }) {
+	const query = ` UPDATE users SET first_name = ?, last_name = ?, email = ?, password = ? WHERE id = ? `;
+
+	await connection.execute(query, [firstName, lastName, email, password, id]);
+	return findById(id);
+}
+
+module.exports = { formatUser, create, findAll };
