311828a21299b0e7b9548a08e0760eb9b1cc518f
Alline <alline_franciely@hotmail.com>
2022-07-08 11:30:02 -0300

    exercícios do dia

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/books-exercise/services/BookService.js
@@ -0,0 +1,54 @@
+const { Book } = require('../models');
+
+const getAll = async () => {
+  const books = await Book.findAll({
+    order: [['title', 'ASC']],
+  });
+  return books;
+};
+
+const getById = async (id) => {
+  const book = await Book.findByPk(id);
+  return book;
+};
+
+const create = async ({ title, author, pageQuantity }) => {
+  const book = await Book.create({ title, author, pageQuantity });
+  return book;
+};
+
+const update = async (is, { title, author, pageQuantity }) => {
+  const [updated] = await Book.update(
+    {
+      title,
+      author,
+      pageQuantity,
+    },
+    { where: { id } },
+  );
+  return updated;
+};
+
+const remove = async(id) => {
+  const removed = await Book.destroy(
+    { where: { id } },
+  );
+  return removed;
+};
+
+const getByAuthor = async (author) => {
+  const books = await Book.findAll({ 
+    where: { author },
+    order: [['title', 'ASC']],
+  });
+  return books;
+};
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+  update,
+  remove,
+  getByAuthor,
+};
