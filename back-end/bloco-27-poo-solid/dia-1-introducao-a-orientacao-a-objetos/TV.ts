edd98ed8b14c560a6df52bbf311a6ca3028f00a2
Alline <alline_franciely@hotmail.com>
2022-08-03 14:43:19 -0300

    exercício 7

--- back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/TV.ts
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/TV.ts
@@ -21,6 +21,19 @@ class TV {
     console.log(`TV ${this._brand}, ${this._size}", resolution: ${this._resolution} \n\
     available connections: ${this._connections}`,);
   }
+
+  get connectedTo(): string | undefined {
+    return this._connectedTo;
+  }
+
+  set connectedTo(value: string | undefined) {
+    if (!value || this._connections.includes(value)) {
+      this._connectedTo = value;
+      console.log(this._connectedTo);
+    } else {
+      console.log('Sorry, connection unavailable!');
+    }
+  }
 }
 
 const tv1 = new TV('LG', 32, '4K', ['HDMI', 'Ethernet', 'Wifi']);
