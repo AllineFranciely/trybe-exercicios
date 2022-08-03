3abbdcfea4386813a11e00c811a5dd652c3edbd5
Alline <alline_franciely@hotmail.com>
2022-08-03 14:36:50 -0300

    exercícios 1 e 2

---
+++ back-end/bloco-27-poo-solid/dia-1-introducao-a-orientacao-a-objetos/TV.ts
@@ -0,0 +1,24 @@
+class TV {
+  brand: string;
+  size: number;
+  resolution: string;
+  connections: string[];
+  connectedTo?: string;
+
+  constructor(
+    brand: string,
+    size: number,
+    resolution: string,
+    connections: string[],
+  ) {
+    this.brand = brand;
+    this.size = size;
+    this.resolution = resolution;
+    this.connections = connections;
+  }
+
+  turnOn():void {
+    console.log(`TV ${this.brand}, ${this.size}", resolution: ${this.resolution} \n\
+    available connections: ${this.connections}`,);
+  }
+}
